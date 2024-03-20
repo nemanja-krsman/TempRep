2a734d2bc5041a48bba6c5efe80bf8587e0fbcbb
nemanja-krsman <65512613+nemanja-krsman@users.noreply.github.com>
2024-03-20 14:35:03 +0100

    Updated

--- GeneratorXML.c
+++ GeneratorXML.c
@@ -1,54 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-// Struktura za čuvanje informacija o funkcijama
-typedef struct {
-    char name[100];
+void generateXML(char input[]) {
+    char functionName[100];
     char argumentName[100];
-    char argumentType[100];
-    char returnType[100];
-    char passing[20];
-} FunctionInfo;
 
-// Funkcija za generisanje XML-a
-void generateXML(FunctionInfo functions[], int numFunctions) {
-    FILE *xmlFile = fopen("functions.xml", "w");
-    if (xmlFile == NULL) {
-        perror("Otvaranje XML fajla nije uspelo");
-        exit(EXIT_FAILURE);
-    }
+    // Razdvajanje ulaznog stringa na ime funkcije i argument
+    sscanf(input, "Rte_Read_%[^(](&%[^)])", functionName, argumentName);
 
-    // Početak XML-a
-    fprintf(xmlFile, "<Functions>\n");
-
-    // Generisanje XML-a za svaku funkciju
-    for (int i = 0; i < numFunctions; i++) {
-        fprintf(xmlFile, "\t<RequiredFunction Name=\"%s\">\n", functions[i].name);
-        fprintf(xmlFile, "\t\t<InputEvent Name=\"%s\" BasicType=\"%s\"/>\n", functions[i].argumentName, functions[i].argumentType);
-        fprintf(xmlFile, "\t\t<Return Type=\"%s\" Passing=\"%s\">\n", functions[i].returnType, functions[i].passing);
-        fprintf(xmlFile, "\t\t\t<InputSignal Name=\"%s_return\" BasicType=\"%s\"/>\n", functions[i].name, functions[i].returnType);
-        fprintf(xmlFile, "\t\t</Return>\n");
-        fprintf(xmlFile, "\t</RequiredFunction>\n");
-    }
-
-    // Kraj XML-a
-    fprintf(xmlFile, "</Functions>\n");
-
-    fclose(xmlFile);
-    printf("XML fajl je uspešno generisan.\n");
+    // Generisanje XML koda
+    printf("<RequiredFunction Name=\"Rte_Read_%s\">\n", functionName);
+    printf("\t<InputParameter Name=\"%s\" Type=\"uint8\" Passing=\"byReference\">\n", argumentName);
+    printf("\t\t<InputSignal Name=\"%s\" BasicType=\"uint8\" />\n", functionName);
+    printf("\t</InputParameter>\n");
+    printf("\t<Return Type=\"Std_ReturnType\" Passing=\"byValue\" Value=\"RTE_E_OK\" />\n");
+    printf("</RequiredFunction>\n");
 }
 
 int main() {
-    // Definisanje informacija o funkcijama
-    FunctionInfo functions[] = {
-        {"Rte_Read_DELIVERY_MODE_INFORMATION_rp_DELIVERY_MODE_INFORMATION", "DELIVERY_MODE_INFORMATION_rp_DELIVERY_MODE_INFORMATION", "uint8", "uint8", "byValue"},
-        // Dodajte ostale funkcije ovde
-    };
-    int numFunctions = sizeof(functions) / sizeof(FunctionInfo);
+    // Ulazni string koji sadrži funkciju i argument
+    char inputString[] = "Rte_Read_FUNKCIJA_rp_FUNKCIJA(&argument)";
 
     // Generisanje XML-a
-    generateXML(functions, numFunctions);
+    generateXML(inputString);
 
     return 0;
 }
